ec2a1e02dd022a96f68c062b64c5b183023e9318
Jason Eveleth <jj11@eveleth.com>
2025-01-13 01:52:37 +0000

    fix script

--- src/analytics.py
+++ src/analytics.py
@@ -4,12 +4,15 @@ import os
 
 # read all csvs in data/ to one dataframe
 def main():
-    if not os.path.exists('data/df.feather'):
-        all_files = glob.glob("data/*.csv")
+    if not os.path.exists('logs/df.feather'):
+        all_files = glob.glob("logs/*.csv")
         df = pd.concat((pd.read_csv(f) for f in all_files))
-        df.to_feather('data/df.feather')
+        df.to_feather('logs/df.feather')
     else:
-        print('using existing dataframe: data/df.feather')
-        df = pd.read_feather('data/df.feather')
+        print('using existing dataframe: logs/df.feather')
+        df = pd.read_feather('logs/df.feather')
 
     print(df.head())
+
+if __name__ == "__main__":
+    main()
